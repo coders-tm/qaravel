021df5fcd26e3ab19cd5e7011572ce38d931606d
DepokSarkar <depokbsmrstu@gmail.com>
2020-10-04 21:53:01 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,34 @@ export default {
   },
   mounted () {
     this.onReady()
+  },
+  watch: {
+    $route (to, from) {
+      this.title = to.name
+    }
+  },
+  computed: {
+    title: {
+      get () {
+        return this.$route.name
+      },
+      set (val) {
+        return val
+      }
+    }
+  },
+  meta () {
+    return {
+      // sets document title
+      title: this.title,
+      // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+      titleTemplate: title => `${title} | ${process.env.APP_NAME}`,
+
+      // <noscript> tags
+      noscript: {
+        default: 'This is content for browsers with no JS (or disabled JS)'
+      }
+    }
   }
 }
 </script>
--- src/boot/components.js
+++ src/boot/components.js
@@ -6,7 +6,7 @@ import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
 export default async ({ Vue }) => {
-  console.log('loading components')
+  console.core('Auto loading components')
   // https://webpack.js.org/guides/dependency-management/#require-context
   const requireComponent = require.context(
     // Look for files in the current directory
