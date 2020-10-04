1474067eddbc2b33986a5a8afed2a406f97dbeb4
DepokSarkar <depokbsmrstu@gmail.com>
2020-10-05 00:25:20 +0530

    initial

--- routes/api.php
+++ routes/api.php
@@ -13,21 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('App\Http\Controllers')->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::post('login', 'AuthController@login');
-        Route::middleware('auth:sanctum')->group(function () {
-          Route::post('logout', 'AuthController@logout');
-          Route::post('me', 'AuthController@me');
-        });
-    });
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::resource('users', 'UserController')->except([
-          'edit', 'create'
-        ]);
-        Route::resource('roles', 'RoleController');
-        Route::resource('parmissions', 'PermissionController');
-    });
+Route::prefix('auth')->group(function () {
+  Route::post('login', 'AuthController@login');
+  Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', 'AuthController@logout');
+    Route::post('me', 'AuthController@me');
+  });
+});
+Route::middleware('auth:sanctum')->group(function () {
+  Route::resource('users', 'UserController')->except([
+    'edit', 'create'
+  ]);
+  Route::resource('roles', 'RoleController');
+  Route::resource('parmissions', 'PermissionController');
 });
 
 
