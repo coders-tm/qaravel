b649db5312960d8788e7563c712362d1a4e8220e
Dipak Sarkar <hello@dipaksarkar.in>
2022-08-18 18:32:04 +0530

    updated style

--- quasar.config.js
+++ quasar.config.js
@@ -30,7 +30,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: ["log", "app", "base-components", "auth"],
+    boot: ["log", "app", "base-components", "font-awesome-pro", "auth"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
---
+++ src/boot/font-awesome-pro.js
@@ -0,0 +1,8 @@
+// required
+import '@fortawesome/fontawesome-pro/css/fontawesome.css';
+import '@fortawesome/fontawesome-pro/css/light.css';
+import '@fortawesome/fontawesome-pro/css/solid.css';
+import '@fortawesome/fontawesome-pro/css/duotone.css';
+// do you want these too?
+import '@fortawesome/fontawesome-pro/css/brands.css';
+import '@fortawesome/fontawesome-pro/css/regular.css';
