eec077984c7dec046444f42c944866dcd96a9b05
Dipak Sarkar <hello@dipaksarkar.in>
2022-08-18 14:40:56 +0530

    remove bak files

--- src.bak/pages/BlankPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <q-page padding class="flex flex-center">
-    <div class="row text-center" style="width: 300px">
-      This page is blank, content is currently being developed.
-    </div>
-  </q-page>
-</template>
