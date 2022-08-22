cf45bd74abe3425cf022c08b5d6fd55612fb1720
Dipak Sarkar <hello@dipaksarkar.in>
2022-08-22 07:53:52 +0530

    updated logging system

--- src/boot/log.js
+++ src/boot/log.js
@@ -4,7 +4,7 @@ export default () => {
       origConsole = {};
     }
 
-    var isDebug = process.env.APP_DEBUG === 'true',
+    var isDebug = process.env.DEV,
       isSaveLog = false,
       logArray = {
         logs: [],
@@ -16,78 +16,92 @@ export default () => {
 
     return {
       core: function () {
-        this.addLog(arguments, 'core');
+        this.addLog(arguments, "core");
         Array.prototype.unshift.call(
           arguments,
-          '%c[Core]',
-          'color: #fff; background: #ea1d75; font-weight:700;padding:5px'
+          "%c[Core]",
+          "color: #fff; background: #ea1d75; font-weight:700;padding:5px"
         );
         origConsole.log && origConsole.log.apply(origConsole, arguments);
       },
       func: function () {
-        this.addLog(arguments, 'funcs');
-        arguments[0] = '%c[Func]%c ' + arguments[0];
+        this.addLog(arguments, "funcs");
+        arguments[0] = "%c[Func]%c " + arguments[0];
         Array.prototype.splice.call(
           arguments,
           1,
           0,
-          'color: #fff; background:#CC66CC; font-weight:700;padding:5px',
-          'color: #CC66CC; font-weight:700;'
+          "color: #fff; background:#CC66CC; font-weight:700;padding:5px",
+          "color: #CC66CC; font-weight:700;"
         );
-        isDebug && origConsole.log && origConsole.log.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.log &&
+          origConsole.log.apply(origConsole, arguments);
       },
       event: function () {
-        this.addLog(arguments, 'events');
-        arguments[0] = '%c[Event]%c ' + arguments[0];
+        this.addLog(arguments, "events");
+        arguments[0] = "%c[Event]%c " + arguments[0];
         Array.prototype.splice.call(
           arguments,
           1,
           0,
-          'color: #fff; background:#FF6600; font-weight:700;padding:5px',
-          'color: #FF6600; font-weight:700;'
+          "color: #fff; background:#FF6600; font-weight:700;padding:5px",
+          "color: #FF6600; font-weight:700;"
         );
-        isDebug && origConsole.log && origConsole.log.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.log &&
+          origConsole.log.apply(origConsole, arguments);
       },
       log: function () {
-        this.addLog(arguments, 'logs');
+        this.addLog(arguments, "logs");
         Array.prototype.unshift.call(
           arguments,
-          '%c[Debug]',
-          'color: #fff; background: #009900; font-weight:700;padding:5px'
+          "%c[Debug]",
+          "color: #fff; background: #009900; font-weight:700;padding:5px"
         );
-        isDebug && origConsole.log && origConsole.log.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.log &&
+          origConsole.log.apply(origConsole, arguments);
       },
       warn: function () {
-        this.addLog(arguments, 'warns');
-        Array.prototype.unshift.call(arguments, '[Warn]');
-        isDebug && origConsole.warn && origConsole.warn.apply(origConsole, arguments);
+        this.addLog(arguments, "warns");
+        Array.prototype.unshift.call(arguments, "[Warn]");
+        isDebug &&
+          origConsole.warn &&
+          origConsole.warn.apply(origConsole, arguments);
       },
       error: function () {
-        this.addLog(arguments, 'errors');
+        this.addLog(arguments, "errors");
         Array.prototype.unshift.call(
           arguments,
-          '%c[Error]',
-          'color: #fff; background: #990000; font-weight:700;padding:5px'
+          "%c[Error]",
+          "color: #fff; background: #990000; font-weight:700;padding:5px"
         );
-        isDebug && origConsole.error && origConsole.error.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.error &&
+          origConsole.error.apply(origConsole, arguments);
       },
       info: function () {
-        this.addLog(arguments, 'infos');
+        this.addLog(arguments, "infos");
         Array.prototype.unshift.call(
           arguments,
-          '%c[Info]',
-          'color: #fff; background:#0066FF; font-weight:700;padding:5px'
+          "%c[Info]",
+          "color: #fff; background:#0066FF; font-weight:700;padding:5px"
         );
-        isDebug && origConsole.info && origConsole.info.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.info &&
+          origConsole.info.apply(origConsole, arguments);
       },
       trace: function () {
-        this.addLog(arguments, 'trace');
+        this.addLog(arguments, "trace");
         Array.prototype.unshift.call(
           arguments,
-          '%c[Trace]',
-          'color: #fff; background:#FFCC00;font-weight:700;padding:5px'
+          "%c[Trace]",
+          "color: #fff; background:#FFCC00;font-weight:700;padding:5px"
         );
-        isDebug && origConsole.trace && origConsole.trace.apply(origConsole, arguments);
+        isDebug &&
+          origConsole.trace &&
+          origConsole.trace.apply(origConsole, arguments);
       },
       debug: function (bool) {
         isDebug = bool;
@@ -99,7 +113,7 @@ export default () => {
         if (!isSaveLog) {
           return;
         }
-        logArray[array || 'logs'].push(args);
+        logArray[array || "logs"].push(args);
       },
       logArray: function () {
         return logArray;
