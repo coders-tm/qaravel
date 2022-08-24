3788ed0a10e79a7a265a7269daaf9dedca24ac9f
Dipak Sarkar <hello@dipaksarkar.in>
2022-08-24 14:16:11 +0530

    updated app id

--- quasar.config.js
+++ quasar.config.js
@@ -232,7 +232,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "quasar-vite",
+        appId: "qaravel",
       },
     },
 
