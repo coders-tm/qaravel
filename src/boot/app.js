aba8abc6eb4ec584dba35b8dc953201c5ff5bb14
Dipak Sarkar <hello@dipaksarkar.in>
2023-01-18 11:27:22 +0530

    added axios interceptors for request and response

--- src/boot/app.js
+++ src/boot/app.js
@@ -24,6 +24,34 @@ export default boot(async ({ app, store }) => {
   //    axios setup    //
   axios.defaults.baseURL = process.env.API_URL;
 
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      console.log("request error", error);
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axios.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      console.log("response error", error);
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
+
   //    core prototypes setup    //
   core.$axios = axios;
   core.$store = store;
--- src/services/network.js
+++ src/services/network.js
@@ -1,7 +1,36 @@
 import { Network } from "@capacitor/network";
+import { Notify } from "quasar";
+
+const OFFLINE_MESSAGE = "You are currently offline.";
+const ONLINE_MESSAGE = "Your Internet connection was restored.";
+
+let notificaion;
+
+export const showStatusMessage = ({ connected }) => {
+  const message = connected ? ONLINE_MESSAGE : OFFLINE_MESSAGE;
+  const icon = connected ? "fas fa-wifi" : "fas fa-wifi-slash";
+  const timeout = connected ? 5000 : 0;
+
+  // remove offline notification
+  if (connected && notificaion) notificaion();
+
+  notificaion = Notify.create({
+    message: message,
+    icon: icon,
+    timeout: timeout,
+    actions: [
+      {
+        icon: "fas fa-circle-xmark",
+        dense: true,
+        color: "white",
+      },
+    ],
+  });
+};
 
 Network.addListener("networkStatusChange", (status) => {
   console.log("Network status changed", status);
+  showStatusMessage(status);
 });
 
 export const getNetworkStatus = async () => {
@@ -14,4 +43,5 @@ export default {
     console.func("network/core:init()", arguments);
   },
   getNetworkStatus,
+  showStatusMessage,
 };
